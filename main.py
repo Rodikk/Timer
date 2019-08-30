c5442930b37eb59297ad754b774e19434e096863
Rodikk <39002082+Rodikk@users.noreply.github.com>
2019-08-30 13:03:19 +0200

    First attempt to add split
    Not working split button added.
    Function is not finished as I have to rethink the structure of the way I define variables.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import Tk,Label,Button,Frame
 from datetime import datetime
 proceso=0
- 
+i = 0
+
 def iniciar():
     global proceso
     global tini
@@ -21,7 +22,15 @@ def ejecutar():
   
   #Calls itself recursively every ms updating the time on the timer
   proceso=time.after(1, ejecutar)
-  
+
+def split():
+  global proceso
+  global i
+  i += 1
+  timesplit = time
+  timesplit = Label(root, fg='red', width=20, font=("","14"))
+  timesplit.pack()
+
 def parar():
     global proceso
     time.after_cancel(proceso)
@@ -36,8 +45,12 @@ time.pack()
 frame=Frame(root)
 btnIniciar=Button(frame, fg='blue', text='Iniciar', command=iniciar)
 btnIniciar.grid(row=1, column=1)
+btnSplit=Button(frame, fg='blue', text='Split', command=split)
+btnSplit.grid(row=1, column=2)
 btnParar=Button(frame, fg='blue', text='Parar', command=parar)
-btnParar.grid(row=1, column=2)
+btnParar.grid(row=1, column=3)
+btnSalir=Button(frame, fg='blue', text='Salir', command=quit)
+btnSalir.grid(row=1, column=4)
 frame.pack()
  
 root.mainloop()
