a9b70e68d91c20a4e1b855fe011baade273e7af6
Rodikk <39002082+Rodikk@users.noreply.github.com>
2019-08-31 11:38:27 +0200

    Added Exit button and format time
    Created strfdelta function which gives the desired format to the time and added an exit button to terminate the program

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ def ejecutar():
   global proceso
   global tini
   
+  now = datetime.now()-tini
   # Write the time
-  time['text'] = datetime.now()-tini
+  date = strfdelta(now, "{hours}:{minutes}:{seconds}")
+  cs = int(now.microseconds/10000)
+  time['text'] = str(date)+":"+str(cs)
   
   #Calls itself recursively every ms updating the time on the timer
   proceso=time.after(1, ejecutar)
@@ -25,10 +28,17 @@ def ejecutar():
 def parar():
     global proceso
     time.after_cancel(proceso)
+   
+def strfdelta(tdelta, fmt):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return fmt.format(**d)
  
 root = Tk()
 root.title('Cronometro')
- 
+root.geometry('225x400') # anchura x altura
+
 time = Label(root, fg='red', width=20, font=("","18"))
 time.pack()
  
@@ -38,6 +48,8 @@ btnIniciar=Button(frame, fg='blue', text='Iniciar', command=iniciar)
 btnIniciar.grid(row=1, column=1)
 btnParar=Button(frame, fg='blue', text='Parar', command=parar)
 btnParar.grid(row=1, column=2)
+btnSalir=Button(frame, fg='blue', text='Salir', command=quit)
+btnSalir.grid(row=1, column=3)
 frame.pack()
  
 root.mainloop()
